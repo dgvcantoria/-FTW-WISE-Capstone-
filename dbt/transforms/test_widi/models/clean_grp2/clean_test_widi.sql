3c6d1f12dd6520a7095f5dc54ad2ca0e6890cfff
Marjorie <marjorie.crtz@yahoo.com>
2025-10-29 20:59:32 +0800

    revert changes

--- dbt/transforms/test_widi/models/clean_grp2/clean_test_widi.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized="table", schema="clean_grp2") }}
-
--- Source columns are already Nullable with correct types.
--- Keep them as-is to preserve nullability and avoid insert errors.
-
-select *
-from {{ source('raw_grp2', 'raw___test_widi') }}
--- dbt/transforms/test_widi/models/mart_grp2/mart_test_widi.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized="table", schema="mart_grp2") }}
-
-select *
-from {{ ref('clean_test_widi') }}
